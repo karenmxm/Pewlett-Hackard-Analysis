d7bb8dc4d90bec11d7fd16a6226b168ab1151c5b
mengxiaomeng01 <mengxiao23@gmail.com>
2020-05-13 12:12:39 -0500

    message

--- Queries/schema.sql
+++ Queries/schema.sql
@@ -37,8 +37,8 @@ CREATE TABLE salaries (
 );
 
 CREATE TABLE dept_emp (
-dept_no VARCHAR(4) NOT NULL,
-	emp_no INT NOT NULL,
+    emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
@@ -51,6 +51,53 @@ CREATE TABLE titles (
   title VARCHAR(40) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)  
 );
+
+SELECT * FROM employees;
+DROP TABLE titles CASCADE;
+DROP TABLE dept_emp CASCADE;
+
+/*select count(*)
+from
+*/(
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31'
+/*)src;*/
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+	
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+	
+-- Retirement eligibility
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+	
+-- Number of employees retiring
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+	
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');	
+	
+SELECT * FROM retirement_info;	
